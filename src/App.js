30e784395c5751e82c05fda8656c97b6ecb0131e
Eloy Gontijo Martins <eloy@sgeinformatica.com.br>
2019-08-30 18:01:32 -0300

    modulo08: useState e useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return <h1>SGE INFORMÁTICA</h1>;
+  const [tech, setTech] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  // didMount (executa apenas uma vez [])
+  useEffect(() => {
+    const itens = localStorage.getItem('tech');
+    if (itens) {
+      setTech(JSON.parse(itens));
+    }
+  }, []);
+
+  // didUpdate (fica monitorando [tech])
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
+  function handleAdd() {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }
+
+  return (
+    <>
+      <ul>
+        {tech.map((item, index) => (
+          <li key={item}>{`${index + 1} - ${item}`}</li>
+        ))}
+      </ul>
+      <input
+        value={newTech}
+        onChange={event => setNewTech(event.target.value)}
+      />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
 }
 
 export default App;
